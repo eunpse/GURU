90a308aa95fb8663c7c2238fc89aee1cf6056bcd
seeun <eunpse@gmail.com>
2023-01-30 16:40:38 +0900

    fragment #1

--- app/src/main/java/com/example/guru/MainActivity.kt
+++ app/src/main/java/com/example/guru/MainActivity.kt
@@ -5,17 +5,56 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.guru.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnMove: Button
 
+    // 프레그먼트 선언
+
+    lateinit var binding: ActivityMainBinding
+
+    companion object {
+
+        const val TAG: String = "로그"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "MainActivity - onCreate() called")
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // 레이아웃과 연결
+        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val bottomBar = binding.bnvMain
+
+        bottomBar.setOnItemSelectedListener { item ->
+            Log.d(TAG,"MainActivity - setOnItemSelectedListener() called")
+            changeFragment(
+                when (item.itemId) {
+                    R.id.menu_calendar-> {
+                        CalendarFragment()
+                    }
+                    R.id.menu_setting->{
+                        SettingFragment()
+                    }
+                    else -> {
+                        DogNoteFragment()
+                    }
+                }
+            )
+            true
+        }
+        bottomBar.selectedItemId = R.id.menu_dog_note
 
         btnMove = findViewById<Button>(R.id.move_btn)
 
@@ -30,6 +69,13 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fl_container, fragment)
+            .commit()
     }
 
     //저장소 권한 얻기
